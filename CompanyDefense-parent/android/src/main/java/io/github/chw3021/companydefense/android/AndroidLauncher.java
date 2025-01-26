72876ead0557e1aed943710ca24c960bbf81aabf
chw3021 <ckgusdnr1205@gmail.com>
2025-01-26 19:41:58 +0900

    250126

--- CompanyDefense-parent/android/src/main/java/io/github/chw3021/companydefense/android/AndroidLauncher.java
+++ CompanyDefense-parent/android/src/main/java/io/github/chw3021/companydefense/android/AndroidLauncher.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-//import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseApp;
 
 import io.github.chw3021.companydefense.Main;
-//import io.github.chw3021.companydefense.android.firebase.AndroidFirebaseService;
-//import io.github.chw3021.companydefense.firebase.FirebaseService;
+import io.github.chw3021.companydefense.android.firebase.AndroidFirebaseService;
+import io.github.chw3021.companydefense.firebase.FirebaseService;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
@@ -16,9 +16,9 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //FirebaseApp.initializeApp(this);
+        FirebaseApp.initializeApp(this);
         // 반드시 Context를 전달해야 함
-       // FirebaseService firebaseService = new AndroidFirebaseService();
+        FirebaseService firebaseService = new AndroidFirebaseService();
 
 
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
@@ -26,7 +26,7 @@ public class AndroidLauncher extends AndroidApplication {
         initialize(new Main(), configuration);
 
 
-        //initialize(new Main(firebaseService), configuration);
+        initialize(new Main(firebaseService), configuration);
     }
 
 }
--- CompanyDefense-parent/android/src/main/java/io/github/chw3021/companydefense/android/firebase/AndroidFirebaseService.java
+++ CompanyDefense-parent/android/src/main/java/io/github/chw3021/companydefense/android/firebase/AndroidFirebaseService.java
@@ -1,70 +1,73 @@
-//package io.github.chw3021.companydefense.android.firebase;
-//
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//
-//import io.github.chw3021.companydefense.firebase.FirebaseService;
-//import io.github.chw3021.companydefense.firebase.FirebaseCallback;
-//
-//public class AndroidFirebaseService implements FirebaseService {
-//    private final FirebaseAuth auth;
-//    private final FirebaseDatabase database;
-//
-//    public AndroidFirebaseService() {
-//        auth = FirebaseAuth.getInstance();
-//        database = FirebaseDatabase.getInstance();
-//    }
-//
-//    @Override
-//    public <T> void fetchData(String path, Class<T> type, FirebaseCallback<T> callback) {
-//        DatabaseReference ref = database.getReference(path);
-//        ref.get().addOnSuccessListener(snapshot -> {
-//            T data = snapshot.getValue(type);
-//            callback.onSuccess(data);
-//        }).addOnFailureListener(callback::onFailure);
-//    }
-//
-//    @Override
-//    public <T> void saveData(String path, T data, FirebaseCallback<Void> callback) {
-//        DatabaseReference ref = database.getReference(path);
-//        ref.setValue(data).addOnSuccessListener(aVoid -> callback.onSuccess(null))
-//                .addOnFailureListener(callback::onFailure);
-//    }
-//
-//    @Override
-//    public void login(String email, String password, FirebaseCallback<Void> callback) {
-//        auth.signInWithEmailAndPassword(email, password)
-//                .addOnSuccessListener(authResult -> callback.onSuccess(null))
-//                .addOnFailureListener(callback::onFailure);
-//    }
-//
-//    @Override
-//    public void loginWithProvider(String provider, String token, FirebaseCallback<Void> callback) {
-//        AuthCredential credential = null;
-//        if (provider.equals("google")) {
-//            credential = GoogleAuthProvider.getCredential(token, null);
-//        }
-//        FirebaseAuth.getInstance().signInWithCredential(credential)
-//            .addOnCompleteListener(task -> {
-//                if (task.isSuccessful()) {
-//                    callback.onSuccess(null);
-//                } else {
-//                    callback.onFailure(task.getException());
-//                }
-//            });
-//    }
+package io.github.chw3021.companydefense.android.firebase;
 
-//    @Override
-//    public void logout() {
-//        auth.signOut();
-//    }
-//
-//    @Override
-//    public String getCurrentUserId() {
-//        if (auth.getCurrentUser() != null) {
-//            return auth.getCurrentUser().getUid();
-//        }
-//        return null;
-//    }
-//}
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import io.github.chw3021.companydefense.firebase.FirebaseService;
+import io.github.chw3021.companydefense.firebase.FirebaseCallback;
+
+public class AndroidFirebaseService implements FirebaseService {
+    private final FirebaseAuth auth;
+    private final FirebaseDatabase database;
+
+    public AndroidFirebaseService() {
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+    }
+
+    @Override
+    public <T> void fetchData(String path, Class<T> type, FirebaseCallback<T> callback) {
+        DatabaseReference ref = database.getReference(path);
+        ref.get().addOnSuccessListener(snapshot -> {
+            T data = snapshot.getValue(type);
+            callback.onSuccess(data);
+        }).addOnFailureListener(callback::onFailure);
+    }
+
+    @Override
+    public <T> void saveData(String path, T data, FirebaseCallback<Void> callback) {
+        DatabaseReference ref = database.getReference(path);
+        ref.setValue(data).addOnSuccessListener(aVoid -> callback.onSuccess(null))
+                .addOnFailureListener(callback::onFailure);
+    }
+
+    @Override
+    public void login(String email, String password, FirebaseCallback<Void> callback) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> callback.onSuccess(null))
+                .addOnFailureListener(callback::onFailure);
+    }
+
+    @Override
+    public void loginWithProvider(String provider, String token, FirebaseCallback<Void> callback) {
+        AuthCredential credential = null;
+        if (provider.equals("google")) {
+            credential = GoogleAuthProvider.getCredential(token, null);
+        }
+        assert credential != null;
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    callback.onSuccess(null);
+                } else {
+                    callback.onFailure(task.getException());
+                }
+            });
+    }
+
+    @Override
+    public void logout() {
+        auth.signOut();
+    }
+
+    @Override
+    public String getCurrentUserId() {
+        if (auth.getCurrentUser() != null) {
+            return auth.getCurrentUser().getUid();
+        }
+        return null;
+    }
+}
